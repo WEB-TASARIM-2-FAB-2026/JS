9518c1b96412101752d0f12c4c2af5c0565cdce4
erkanturk <Refres98@gmail.com>
2026-02-25 21:41:51 +0300

    degiskenler

---
+++ degiskenler.js
@@ -0,0 +1,14 @@
+console.log("Merhaba Dünya!")
+
+// DEĞİŞKEN TANIMLAMAK:
+// var <değişkenin adı> = <değişkenin değeri>
+
+
+var ad = "Cem" // String: Yani metin demektir. Tırnak içinde yazılır.
+var yas = 73 // Number: sayı demektir. Sayısal ifadeler için kullanılır.
+var yasiyorMu = true // - true - false : Boolean : yani sadece doğru veya yanlış olan değişkenler için kullanılır.
+
+
+console.log("İsim: ", ad)
+console.log("Yaş: ", yas)
+console.log("Yaşıyor mu: ", yasiyorMu)
