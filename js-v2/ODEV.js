6d179e957c528edd0f25bc300da1ef002ad252b8
bedirhan-1 <bedirhan9828@gmail.com>
2026-03-11 21:21:35 +0300

    11 mart 2026

--- js-v2/ODEV.js
+++ js-v2/ODEV.js
@@ -1,2 +1,22 @@
 // sayılardan oluşan bir dizi tanımlayın, içinde negatif ve pozitif sayılar olabilir, bu dizinin bütün
 // elemanlarının toplamını ekrana yazdıran kod
+
+// var a = [1, -5, 10, 96, -81, 104]
+// var toplam = 0
+
+// // a[0] + a[1] + a[2] .. + a[5]
+
+// for(var sayac = 0; sayac < a.length; sayac++){
+//     toplam = toplam + a[sayac]
+// }
+
+
+// console.log(toplam)
+
+
+//              1   2   3   4   5   6
+// sayac:       0   1   2   3   4   5
+// toplam:      1  -4   6  102  21 125
+const arr = [1,34,52,78,65,93];
+const result = arr.reduce((total,current) => total + current, -1500);
+console.log(result)
--- js-v2/array.js
+++ js-v2/array.js
@@ -1,13 +1,35 @@
 // ARRAY METODLARI
 
-var a = ["elma", "armut", "kel mahmut"] // a: string[] 
+// var a = ["elma", "armut", "karpuz"] // a: string[] 
 
 // console.log(a)
 // a.push("nar") // sondan eleman ekler
 
 // a.pop()
 
-console.log(a[2][2])
+// var b = a.shift()
 
-console.log(a.length)
+// var b = a.unshift("avokado")
 
+// var b = a.slice(0,2)
+
+
+// ARRAYLERDE DÖNGÜ FONKSİYONLARI
+
+var meyveler = ["elma", "armut", "muz", "çilek", "karpuz"];
+
+// meyveler.forEach(function(meyve){
+//     console.log(meyve)
+// })
+
+// meyveler.forEach((meyve) => console.log(meyve))
+
+// meyveler.forEach((meyve) => {
+//     if (meyve.length > 4) {
+//         console.log(meyve)
+//     }
+// })
+
+// MAP FONKSİYONU (ÖNEMLİ)
+
+meyveler.map(meyve => console.log(meyve))
